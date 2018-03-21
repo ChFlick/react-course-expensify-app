a871be93fa1276c9395a23bd65915d5082e107ad
Christoph Flick <christophflick@gmx.de>
2018-03-21 12:48:58 +0100

    Added test for hidden expenses

--- src/components/ExpensesSummary.jsx
+++ src/components/ExpensesSummary.jsx
@@ -9,15 +9,25 @@ import getExpensesTotal from '../selectors/expenses-total';
 
 numeral.locale('de');
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+export const ExpensesSummary = ({ expenseCount, hiddenExpenseCount, expensesTotal }) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
     const formattedTotal = numeral(expensesTotal / 100).format('0.0[,]00$');
+
+    const hiddenExpenseWord = hiddenExpenseCount === 1 ? 'expense' : 'expenses';
+    const hiddenExpensePronoun = hiddenExpenseCount === 1 ? 'is' : 'are';
 
     return (
         <div className="page-header">
             <div className="content-container">
-                <h1 className="page-header__title">Viewing <span>{expenseCount} </span>
-                {expenseWord} totalling <span>{formattedTotal}</span></h1>
+                <h1 className="page-header__title">
+                    Viewing <span>{expenseCount} </span> {expenseWord} totalling <span>{formattedTotal}</span>
+                </h1>
+                <h3 className="page-header__subtitle">
+                    {hiddenExpenseCount > 0 ?
+                        `There ${hiddenExpensePronoun} ${hiddenExpenseCount} ${hiddenExpenseWord} hidden by filters.` :
+                        'All expenses visible.'
+                    }
+                </h3>
                 <div className="page-header__actions">
                     <Link className="button" to="/create">Add Expense</Link>
                 </div>
@@ -31,6 +41,7 @@ const matchStateToProps = (state) => {
 
     return {
         expenseCount: visibleExpenses.length,
+        hiddenExpenseCount: state.expenses.length - visibleExpenses.length,
         expensesTotal: getExpensesTotal(visibleExpenses),
     };
 };
